1a6a37dbc746ef57490642d5b0a0d5f623b8b684
passportpowell <otisofro@gmail.com>
2022-08-15 10:03:33 +0000

    add js for pop up window

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-console.log("connected")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You Clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gameType}`);
+            }
+            
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
